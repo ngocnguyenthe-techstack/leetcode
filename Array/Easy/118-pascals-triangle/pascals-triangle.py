67c4969ef5d922d9fa26d070c1be546eecb51851
Ngoc Nguyen The <ngocnguyenthe.work@gmail.com>
2025-03-24 08:54:30 +0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (87.61%) - LeetSync

---
+++ Array/Easy/118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascal_triangle = [[1] * (i + 1) for i in range(numRows)]
+
+        for i in range(2,numRows):
+            for j in range(1,i):
+                pascal_triangle[i][j] = pascal_triangle[i-1][j-1] + pascal_triangle[i-1][j]
+
+        return pascal_triangle
